94f50caabd6a9ecb4451ec0ab6959189af499953
Shivam4sk <shivamsk.sd@gmail.com>
2026-09-15 16:51:46 +0530

    section 4

---
+++ src/main/java/org/softoHiest/eCommerce/UserController.java
@@ -0,0 +1,54 @@
+package org.softoHiest.eCommerce;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private  UserService userService;
+
+    @GetMapping("/getAllusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.fetchAllUsers());// Logic to retrieve all users from the database
+    }
+
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        //return ResponseEntity.ok(userService.fetchUserById(id));
+
+        return userService.fetchUserById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return ResponseEntity.ok("User added sucessfully.");
+    }
+
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updatUser){
+        boolean update = userService.updateUser(id,updatUser);
+        if(update)
+            return ResponseEntity.ok("Update Successfully.");
+        return  ResponseEntity.notFound().build();
+    }
+
+
+}
